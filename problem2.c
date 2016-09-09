42899fca11d8d338a9d7f833faae3eb2edbc862a
GrantWishes <grant-gertsen@uiowa.edu>
2016-09-08 21:04:39 -0500

    Getting closer to finishing debugging, don't want to break anything!

--- problem2.c
+++ problem2.c
@@ -14,10 +14,7 @@ void add_and_print(char* num1, char* num2) {
 	for(int i = 0; num2[i] != '\0'; i++) {
 		index2++;
 	} 
-	char c = '4'; char d = '2';
-	int e = c - d;
-
-
+//printf("Index 1: %d Index 2: %d \n",index1,index2);
 	
 	int sum[MAX_NUM_LENGTH+1]; // just incase carry
 	int temp;		   // hold the number
@@ -25,11 +22,10 @@ void add_and_print(char* num1, char* num2) {
 	int carry = 0;
 	
 	while(index1 >= 0 && index2 >= 0) {
-		printf("index1 is %d and index2 is %d\n",index1,index2);
 		temp =(int)(num1[index1]-'0') +(int)(num2[index2]-'0') + carry;
-		printf("Adding %c and %c and %d  = %d\n",num1[index1],num2[index2],carry,temp);
 		if(temp >= 10) {
 			temp = temp % 10;
+			printf("Carry occurring!\n");
 			carry = 1;
 		}
 		else {
@@ -37,7 +33,8 @@ void add_and_print(char* num1, char* num2) {
 		}
 		--index1; --index2;
 		sum[sumIndex++] = temp;
-}
+printf("Index1: %d Index2: %d SumIndex: %d Number added: %d\n",index1,index2,sumIndex,temp);
+	}	
 
 
 // STATE OF THE CODE: Right now, your code is wrong. And backwards, but mainly wrong. Figure your shit out
@@ -49,29 +46,39 @@ void add_and_print(char* num1, char* num2) {
 	char* finalNum;
 
 	if(index1 == index2) {
+		finalIndex = 0;
 		sum[sumIndex] = -1;	
 	} 
 	else {
-		if(index1 == 0) {
-			finalIndex = index2;
-			strcpy(finalNum,num2);
+		if(index1 == -1 ) {
+			printf("Second number bigger!\n");		
+//			finalIndex = index2;
+//			strcpy(finalNum,num2);
+			while(index2 >= 0) {
+				temp = (int)(num2[index2]-'0') + carry;
+				printf("index2: %d number added: %d carry %d\n", index2, temp, carry);
+				sum[sumIndex++] = temp;
+				carry = 0;
+				--index2;
+			}
 		}
 		else {
-			finalIndex = index1;
-			strcpy(finalNum,num1);
+			printf("First number bigger!\n");
+			while(index1 >=0) {
+				temp = (int)(num1[index1]-'0') + carry;
+				sum[sumIndex++] = temp;
+				carry = 0;
+				--index1;
+			}
 		}
-		printf("This should not be zero: %d %d %d\n",index1, index2, finalIndex);
 	}
-	while(finalIndex > 0) {
-		sum[sumIndex++] = finalNum[finalIndex--] + carry;
-		carry = 0;
-	}
-	sum[sumIndex] = -1;
 
-	for(int i = 0; sum[i] != -1; i++){
-		printf("%d",sum[i]);
-	}
-	printf("\n");
+//	sum[sumIndex] = -1;
+//
+//	for(int i = 0; sum[i] != -1; i++){
+//		printf("%d",sum[i]);
+//	}
+//	printf("\n");
 	
 //	printf("Lengths are %d and %d\n", counter1, counter2);
 //	So type conversion isn't really required. Just add last digits, take the second one (somehow), have a carry toggle 1 or 0 to add on next one.
